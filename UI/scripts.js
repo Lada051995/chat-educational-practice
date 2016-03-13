b807a51d4eab778ba572f523e5431af35e5b195f
Lada051995 <mladda@mail.ru>
2016-03-13 23:21:07 +0200

    Adding edit messages

--- UI/scripts.js
+++ UI/scripts.js
@@ -6,26 +6,15 @@ function run () {
     var rename = document.getElementById('rename');
     var onOff = document.getElementById('buttonOnOff');
     var deleteMessage = document.getElementById('delete');
+    var editMessage = document.getElementById('edit');
 
     send.addEventListener ('click', EventSend);
     rename.addEventListener('click', EventRename);
     onOff.addEventListener('click', EventOnOff);
     deleteMessage.addEventListener('click', EventDelete);
+    editMessage.addEventListener('click', EventEdit);
 }
 
-function EventSend(evtObj) {
-    var areatext = document.getElementById('myTextArea');
-    var textArea = document.getElementById('nameSurname');
-    if (areatext.value&&textArea.value) {
-        debugger
-        var selected = document.getElementById('sel1');
-        var option = document.createElement("option");
-        option.text = textArea.value + ": " +areatext.value;
-        option.value = selected.length;
-        selected.add(option);
-        areatext.value = "";
-    }
-}
 
 function EventRename(evtObj) {
     var nameSurname = document.getElementById('nameSurname');
@@ -49,6 +38,35 @@ function EventOnOff(evtObj) {
     }
 }
 
+function EventSend(evtObj) {
+    var areatext = document.getElementById('myTextArea');
+    var textArea = document.getElementById('nameSurname');
+    var selected = document.getElementById('sel1');
+    if (areatext.value&&textArea.value) {
+        var ind = 0;
+        for (var i = 0; i < selected.length; i++){
+            var index = document.getElementById('sel1').selectedIndex = i.toString();
+            var select = document.getElementById('sel1')[index];
+            if (select.text.indexOf("*editing process*") != -1){
+                ind = i;
+            }
+        }
+        if (ind == 0){
+            var option = document.createElement("option");
+            option.text = textArea.value + ": " +areatext.value;
+            option.value = selected.length;
+            selected.add(option);
+            areatext.value = "";
+        }
+        else {
+            var select = document.getElementById('sel1')[ind];
+            select.text = textArea.value + ": " +areatext.value + " /Edit";
+            areatext.value="";
+        }
+
+    }
+}
+
 function EventDelete (evtObj) {
     var n_s = document.getElementById('nameSurname');
     var index = document.getElementById('sel1').selectedIndex;
@@ -58,4 +76,26 @@ function EventDelete (evtObj) {
         select.text = "Message has been deleted by " + n_s.value;
     }
 
+}
+
+function EventEdit (evtObj) {
+    var n_s = document.getElementById('nameSurname');
+    var areatext = document.getElementById('myTextArea');
+    var index = document.getElementById('sel1').selectedIndex;
+    var select = document.getElementById('sel1')[index];
+    var subindex = select.text.indexOf (":");
+    if(n_s.value == select.text.substring(0, subindex)&&select.text.indexOf("Message has been deleted by ") == -1){
+        var substring1 = select.text.substring(subindex+1, select.length);
+        if (select.text.indexOf("/Edit") == -1){
+            areatext.value = substring1;
+            select.text = "*editing process*";
+        }
+        else {
+            var ed = select.text.indexOf("/Edit");
+            var substring2 = substring1.substring(0, substring1.length-5);
+            areatext.value = substring2;
+            select.text = "*editing process*";
+        }
+
+    }
 }
